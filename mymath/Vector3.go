7d80b1211882b1fe1b51ba42a853f9e1a9f16909
petr-ujezdsky <petr.ujezdsky@gmail.com>
2021-05-15 22:03:22 +0200

    Implemented Vector3d

--- mymath/Vector3.go
+++ mymath/Vector3.go
@@ -17,3 +17,98 @@ func (v Vector3d) LengthSq() float64 {
 func (v Vector3d) Length() float64 {
 	return math.Sqrt(v.LengthSq())
 }
+
+func (v Vector3d) Add(w Vector3d) Vector3d {
+	return NewVector3d(v.X+w.X, v.Y+w.Y, v.Z+w.Z)
+}
+
+func (v Vector3d) Subtract(w Vector3d) Vector3d {
+	return NewVector3d(v.X-w.X, v.Y-w.Y, v.Z-w.Z)
+}
+
+func (v Vector3d) Multiply(d float64) Vector3d {
+	return NewVector3d(v.X*d, v.Y*d, v.Z*d)
+}
+
+func (v Vector3d) Divide(d float64) Vector3d {
+	inv := 1 / d
+	return v.Multiply(inv)
+}
+
+func (v Vector3d) Negate() Vector3d {
+	return NewVector3d(-v.X, -v.Y, -v.Z)
+}
+
+func (v Vector3d) Abs() Vector3d {
+	return NewVector3d(math.Abs(v.X), math.Abs(v.Y), math.Abs(v.Z))
+}
+
+func (v Vector3d) Normalize() Vector3d {
+	lengthInv := 1 / v.Length()
+	return v.Multiply(lengthInv)
+}
+
+func (v Vector3d) Dot(w Vector3d) float64 {
+	return v.X*w.X + v.Y*w.Y + v.Z*w.Z
+}
+
+func (v Vector3d) Cross(w Vector3d) Vector3d {
+	// always use doubles here!
+	return NewVector3d(
+		v.Y*w.Z-v.Z*w.Y,
+		v.Z*w.X-v.X*w.Z,
+		v.X*w.Y-v.Y*w.X)
+}
+
+func (v Vector3d) Get(component int) float64 {
+	switch component {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	default:
+		return v.Z
+	}
+}
+
+func (v Vector3d) GetMinComponent() float64 {
+	return math.Min(v.X, math.Min(v.Y, v.Z))
+}
+
+func (v Vector3d) GetMaxComponent() float64 {
+	return math.Max(v.X, math.Max(v.Y, v.Z))
+}
+
+func (v Vector3d) GetMaxDimension() int {
+	if v.X > v.Y {
+		if v.X > v.Z {
+			return 0
+		}
+		return 2
+	}
+	if v.Y > v.Z {
+		return 1
+	}
+	return 2
+}
+
+func (v Vector3d) Min(w Vector3d) Vector3d {
+	return NewVector3d(
+		math.Min(v.X, w.X),
+		math.Min(v.Y, w.Y),
+		math.Min(v.Z, w.Z))
+}
+
+func (v Vector3d) Max(w Vector3d) Vector3d {
+	return NewVector3d(
+		math.Max(v.X, w.X),
+		math.Max(v.Y, w.Y),
+		math.Max(v.Z, w.Z))
+}
+
+func (v Vector3d) Permute(x, y, z int) Vector3d {
+	return NewVector3d(
+		v.Get(x),
+		v.Get(y),
+		v.Get(z))
+}
--- mymath/Vector3_test.go
+++ mymath/Vector3_test.go
@@ -25,3 +25,150 @@ func TestLength(t *testing.T) {
 
 	assert.Equal(t, 3.7416573867739413, v.Length())
 }
+func TestAdd(t *testing.T) {
+	v := NewVector3d(1, 2, 3)
+	w := NewVector3d(5, 6, 7)
+
+	res := v.Add(w)
+
+	assert.Equal(t, 6.0, res.X)
+	assert.Equal(t, 8.0, res.Y)
+	assert.Equal(t, 10.0, res.Z)
+}
+func TestSubtract(t *testing.T) {
+	v := NewVector3d(1, 2, 3)
+	w := NewVector3d(5, 6, 7)
+
+	res := v.Subtract(w)
+
+	assert.Equal(t, -4.0, res.X)
+	assert.Equal(t, -4.0, res.Y)
+	assert.Equal(t, -4.0, res.Z)
+}
+
+func TestMultiply(t *testing.T) {
+	v := NewVector3d(1, 2, 3)
+
+	res := v.Multiply(5.0)
+
+	assert.Equal(t, 5.0, res.X)
+	assert.Equal(t, 10.0, res.Y)
+	assert.Equal(t, 15.0, res.Z)
+}
+
+func TestDivide(t *testing.T) {
+	v := NewVector3d(2, 4, -8)
+
+	res := v.Divide(2.0)
+
+	assert.Equal(t, 1.0, res.X)
+	assert.Equal(t, 2.0, res.Y)
+	assert.Equal(t, -4.0, res.Z)
+}
+func TestNegate(t *testing.T) {
+	v := NewVector3d(2, 4, -8)
+
+	res := v.Negate()
+
+	assert.Equal(t, -2.0, res.X)
+	assert.Equal(t, -4.0, res.Y)
+	assert.Equal(t, 8.0, res.Z)
+}
+
+func TestAbs(t *testing.T) {
+	v := NewVector3d(2, 4, -8)
+
+	res := v.Abs()
+
+	assert.Equal(t, 2.0, res.X)
+	assert.Equal(t, 4.0, res.Y)
+	assert.Equal(t, 8.0, res.Z)
+}
+
+func TestNormalize(t *testing.T) {
+	v := NewVector3d(1, 2, 3)
+
+	res := v.Normalize()
+
+	assert.Equal(t, 0.2672612419124244, res.X)
+	assert.Equal(t, 0.5345224838248488, res.Y)
+	assert.Equal(t, 0.8017837257372732, res.Z)
+}
+
+func TestDot(t *testing.T) {
+	v := NewVector3d(-1, -2, -3)
+	w := NewVector3d(4, 5, 6)
+
+	res := v.Dot(w)
+
+	assert.Equal(t, -32.0, res)
+}
+
+func TestCross(t *testing.T) {
+	v := NewVector3d(1, 2, 3)
+	w := NewVector3d(4, 5, 6)
+
+	res := v.Cross(w)
+
+	assert.Equal(t, -3.0, res.X)
+	assert.Equal(t, 6.0, res.Y)
+	assert.Equal(t, -3.0, res.Z)
+}
+
+func TestGet(t *testing.T) {
+	v := NewVector3d(1, 2, 3)
+
+	assert.Equal(t, 1.0, v.Get(0))
+	assert.Equal(t, 2.0, v.Get(1))
+	assert.Equal(t, 3.0, v.Get(2))
+}
+
+func TestGetMinComponent(t *testing.T) {
+	assert.Equal(t, 1.0, NewVector3d(1, 2, 3).GetMinComponent())
+	assert.Equal(t, 2.0, NewVector3d(4, 2, 3).GetMinComponent())
+	assert.Equal(t, 3.0, NewVector3d(5, 5, 3).GetMinComponent())
+}
+
+func TestGetMaxComponent(t *testing.T) {
+	assert.Equal(t, 3.0, NewVector3d(1, 2, 3).GetMaxComponent())
+	assert.Equal(t, 4.0, NewVector3d(4, 2, 3).GetMaxComponent())
+	assert.Equal(t, 5.0, NewVector3d(5, 5, 3).GetMaxComponent())
+}
+
+func TestGetMaxDimension(t *testing.T) {
+	assert.Equal(t, 2, NewVector3d(1, 2, 3).GetMaxDimension())
+	assert.Equal(t, 0, NewVector3d(4, 2, 3).GetMaxDimension())
+	assert.Equal(t, 1, NewVector3d(4, 5, 3).GetMaxDimension())
+	assert.Equal(t, 1, NewVector3d(5, 5, 3).GetMaxDimension())
+}
+
+func TestMin(t *testing.T) {
+	v := NewVector3d(1, 8, 3)
+	w := NewVector3d(4, 5, 6)
+
+	res := v.Min(w)
+
+	assert.Equal(t, 1.0, res.X)
+	assert.Equal(t, 5.0, res.Y)
+	assert.Equal(t, 3.0, res.Z)
+}
+
+func TestMax(t *testing.T) {
+	v := NewVector3d(1, 8, 3)
+	w := NewVector3d(4, 5, 6)
+
+	res := v.Max(w)
+
+	assert.Equal(t, 4.0, res.X)
+	assert.Equal(t, 8.0, res.Y)
+	assert.Equal(t, 6.0, res.Z)
+}
+func TestPermute(t *testing.T) {
+	v := NewVector3d(1, 8, 3)
+
+	res := v.Permute(2, 0, 1)
+
+	assert.Equal(t, 3.0, res.X)
+	assert.Equal(t, 1.0, res.Y)
+	assert.Equal(t, 8.0, res.Z)
+}
