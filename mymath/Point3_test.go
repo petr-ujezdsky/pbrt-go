9562b84a2e742cba9c5e3d9ef46bf54385af35e1
petr-ujezdsky <petr.ujezdsky@gmail.com>
2021-11-14 17:09:35 +0100

    Point3 benchmarks

--- mymath/Point3_test.go
+++ mymath/Point3_test.go
@@ -177,6 +177,246 @@ func TestPoint3_Permute(t *testing.T) {
 	assert.Equal(t, 8.0, res.Z)
 }
 
-func BenchmarkPoint3_Multiply(b *testing.B) {
+func BenchmarkPoint3_NewPoint3(b *testing.B) {
+	var res mymath.Point3
 
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = mymath.NewPoint3(1.0, 2.0, 3.0)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_AddP(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.AddP(p)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_AddV(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+	v := mymath.NewVector3(4, 5, 6)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.AddV(v)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_SubtractP(b *testing.B) {
+	p1 := mymath.NewPoint3(1, 2, 3)
+	p2 := mymath.NewPoint3(4, 5, 6)
+
+	var res mymath.Vector3
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p1.SubtractP(p2)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_SubtractV(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+	v := mymath.NewVector3(4, 5, 6)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.SubtractV(v)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Multiply(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.Multiply(1.0000003)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Distance(b *testing.B) {
+	p1 := mymath.NewPoint3(1, 2, 3)
+	p2 := mymath.NewPoint3(4, 5, 6)
+
+	var res float64
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p1.Distance(p2)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_DistanceSq(b *testing.B) {
+	p1 := mymath.NewPoint3(1, 2, 3)
+	p2 := mymath.NewPoint3(4, 5, 6)
+
+	var res float64
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p1.DistanceSq(p2)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Lerp(b *testing.B) {
+	p1 := mymath.NewPoint3(1, 2, 3)
+	p2 := mymath.NewPoint3(4, 5, 6)
+
+	res := p1
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p1.Lerp(0.9, p2)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_LerpP(b *testing.B) {
+	t := mymath.NewPoint3(0.1, 0.9, 0.5)
+	p1 := mymath.NewPoint3(1, 2, 3)
+	p2 := mymath.NewPoint3(4, 5, 6)
+
+	res := p1
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p1.LerpP(t, p2)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Min(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.Min(p)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Max(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.Max(p)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Floor(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.Floor()
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Ceil(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.Ceil()
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Abs(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = res.Abs()
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Get(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	var res float64
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p.Get(1)
+	}
+
+	assert.NotNil(b, res)
+}
+
+func BenchmarkPoint3_Permute(b *testing.B) {
+	p := mymath.NewPoint3(1, 2, 3)
+
+	res := p
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		res = p.Permute(2, 0, 1)
+	}
+
+	assert.NotNil(b, res)
 }
