0e1a30d6e5405a140264dec081124aba8b15eb79
petr-ujezdsky <petr.ujezdsky@gmail.com>
2021-11-16 20:59:18 +0100

    Clamp function

--- mymath/MyMath.go
+++ mymath/MyMath.go
@@ -10,6 +10,19 @@ func Lerp(t, v1, v2 float64) float64 {
 	return (1-t)*v1 + t*v2
 }
 
+// see https://github.com/mmp/pbrt-v3/blob/master/src/core/pbrt.h#L304
+func Clamp(val, low, high float64) float64 {
+	if val < low {
+		return low
+	}
+
+	if val > high {
+		return high
+	}
+
+	return val
+}
+
 func gamma(n int) float64 {
 	ne := float64(n) * epsilon
 	return ne / (1 - ne)
--- mymath/MyMath_test.go
+++ mymath/MyMath_test.go
@@ -14,3 +14,11 @@ func TestMyMath_Lerp(t *testing.T) {
 	assert.Equal(t, 20.0, mymath.Lerp(1.0, 10.0, 20.0))
 	assert.Equal(t, 40.0, mymath.Lerp(3.0, 10.0, 20.0))
 }
+
+func TestMyMath_Clamp(t *testing.T) {
+	assert.Equal(t, 0.0, mymath.Clamp(-1.0, 0.0, 1.0))
+	assert.Equal(t, 0.0, mymath.Clamp(0.0, 0.0, 1.0))
+	assert.Equal(t, 0.5, mymath.Clamp(0.5, 0.0, 1.0))
+	assert.Equal(t, 1.0, mymath.Clamp(1.0, 0.0, 1.0))
+	assert.Equal(t, 1.0, mymath.Clamp(3.0, 0.0, 1.0))
+}
