c19d6fb550530f3217c3fbd7c14f0fc9cfa15088
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-05-13 23:35:41 +0200

    Shape interfaces

--- shape/Shape.go
+++ shape/Shape.go
@@ -20,6 +20,22 @@ type Intersecter interface {
 	Intersect(ray mymath.Ray, testAlphaTexture bool) (bool, float64, mymath.SurfaceInteraction)
 }
 
+type IntersectPer interface {
+	IntersectP(i Intersecter, ray mymath.Ray, testAlphaTexture bool) bool
+}
+
+type Areaer interface {
+	Area() float64
+}
+
+type IShape interface {
+	ObjectBounder
+	WorldBounder
+	Intersecter
+	IntersectPer
+	Areaer
+}
+
 func NewShape(objectToWorld, worldToObject *mymath.Transform, reverseOrientation bool) *Shape {
 	return &Shape{
 		objectToWorld,
